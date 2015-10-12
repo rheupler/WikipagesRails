40b38bcb879b4621a7da00671e9d70c85c0a5833
ryan heupler & vaughn dice <student@epicodus.com>
2015-10-12 15:48:47 -0700

    improvements to edit city page spec

--- spec/features/edit_city_page_spec.rb
+++ spec/features/edit_city_page_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 describe 'the edit cities page' do
+  before do
+    @country = Country.create(:name => "Matilda")
+    @city = City.create(:name => "Coolio", :country_id => @country.id)
+  end
   it 'edits a city' do
-    country = Country.create(:name => "Matilda")
-    city = City.create(:name => "Coolio", :country_id => country.id)
-    visit country_path(country)
+    visit country_path(@country)
     click_link "Edit"
     fill_in 'Name', :with => "Not Coolio"
     click_on "Update City"
     expect(page).to have_content "Not Coolio"
+  end
+
+  it 'gives an error when no name is entered' do
+    visit country_path(@country)
+    click_link 'Edit'
+    fill_in 'Name', :with => ''
+    click_on 'Update City'
+    expect(page).to have_content 'errors'
   end
 end
