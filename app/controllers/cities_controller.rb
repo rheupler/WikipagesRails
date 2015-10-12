58af33b8eb7f7c37603e72b9ff3f0ae60ffc203b
ryan heupler & vaughn dice <student@epicodus.com>
2015-10-12 15:07:28 -0700

    add show cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,4 +4,20 @@ class CitiesController < ApplicationController
     @city = @country.cities.new
     render :new
   end
+
+  def create
+    @country = Country.find(params[:country_id])
+    @city = @country.cities.new(city_params)
+    if @city.save
+      redirect_to country_path(@city.country)
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def city_params
+      params.require(:city).permit(:name)
+    end
 end
